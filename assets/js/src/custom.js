0b2667ea040212e794bf0c847507dc67d389e4b7
Fernando Tessmann <nandotess85@gmail.com>
2017-07-07 10:58:17 -0300

    removed arrows from testimonials slider

--- assets/js/src/custom.js
+++ assets/js/src/custom.js
@@ -1,11 +1,42 @@
 /**
  * AEPO scripts.
  *
- * @package aepo-lsx-child
+ * @package    aepo-lsx-child
+ * @subpackage scripts
  */
 
-;(function($, window, document, undefined) {
+var lsx_aepo = Object.create( null );
+
+;( function( $, window, document, undefined ) {
 
 	'use strict';
 
-})(jQuery, window, document);
+	var $document    = $( document ),
+		$window      = $( window ),
+		windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight,
+		windowWidth  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+	/**
+	 * Upcate testimonials slick slider (remove arrows)
+	 *
+	 * @package    aepo-lsx-child
+	 * @subpackage scripts
+	 */
+	lsx_aepo.update_testimonials_slick_slider = function() {
+		var $testimonialSlider = $( '#lsx-testimonials-slider' );
+		$testimonialSlider.slick( 'slickSetOption', 'arrows', false, true );
+	};
+
+	/**
+	 * On document ready.
+	 *
+	 * @package    aepo-lsx-child
+	 * @subpackage scripts
+	 */
+	$document.ready( function() {
+
+		lsx_aepo.update_testimonials_slick_slider();
+
+	} );
+
+} )( jQuery, window, document );
